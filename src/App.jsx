1d9ea2d32b58c9d2e842ec5414376b2bc4617cd5
shahzadakram786 <alyakram786@gmail.com>
2025-03-03 22:24:54 +0500

    feed: workin on the parallex 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss"
 import Hero from "./components/hero/Hero";
 
 import Navbar from "./components/navbar/Navbar";
+import Parallex from "./components/parallex/Parallex";
 
 const App = () => {
   return (
@@ -11,9 +12,11 @@ const App = () => {
       <Navbar/>
       <Hero/>
     </section>
-    <section id="Services">Parallex</section>
+    <section id="Services" type="services">
+      <Parallex type="services"/>
+    </section>
     <section>Services</section>
-    <section id="Portfolio">Parallex</section>
+    <section id="Portfolio" type="portfolio">Parallex</section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
