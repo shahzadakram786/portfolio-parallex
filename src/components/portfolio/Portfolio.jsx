ab8eb2cee075d10a1195e03a8b2dde81bf8e0011
shahzadakram786 <alyakram786@gmail.com>
2025-03-04 16:13:04 +0500

    working on

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -48,7 +48,7 @@ const Single = ({ item }) => {
             <div className="imageContainer" ref={ref}>
               <img src={item.img} alt="" />
             </div>
-            <motion.div className="textContainer" >
+            <motion.div className="textContainer" style={{y}}>
               <h2>{item.title}</h2>
               <p>{item.desc}</p>
               <button>See Demo</button>
